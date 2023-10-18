9b36a6f62b7549f5e341d041a6a509282d51f7dd
Armaan Uddin <armaan.uddin@kcl.ac.uk>
2023-10-18 20:27:07 +0100

    Add restrictions to fields in thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.db.models import IntegerField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.CharField()
-    quantity = models.IntegerField()
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False
+    )
+    description = models.CharField(
+        max_length=120,
+        unique=False,
+        blank=False
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ]
+    )
